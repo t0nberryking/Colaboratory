98c3a0256f47628a0d9b0d06fd7415818345925b
t0nberryking <tangchiwang@gmail.com>
2018-09-07 18:55:03 +0800

    Update utils.py
    Changed code to be pasted directly into Jupyter Notebook

--- utils.py
+++ utils.py
@@ -1,44 +1,38 @@
-import os
+## Run code below by directly pasting into Jupyter Notebook
 
 # Authenticate and link to google drive
-def auth():
-  os.system('!apt-get install -y -qq software-properties-common python-software-properties module-init-tools')
-  os.system('!add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null')
-  os.system('!apt-get update -qq 2>&1 > /dev/null')
-  os.system('!apt-get -y install -qq google-drive-ocamlfuse fuse')
-  
-  from google.colab import auth
-  auth.authenticate_user()
-  
-  from oauth2client.client import GoogleCredentials
-  creds = GoogleCredentials.get_application_default()
-  
-  import getpass
-  os.system('!google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL')
-  vcode = getpass.getpass()
-  os.system('!echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}')
 
-def save_all(filename):
-  # tar all files in current directory
-  os.system('!tar -cvf 'filename' .')
-  
-  # save to drive
-  from googleapiclient.http import MediaFileUpload
-  from googleapiclient.discovery import build
+!apt-get install -y -qq software-properties-common python-software-properties module-init-tools
+!add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
+!apt-get update -qq 2>&1 > /dev/null
+!apt-get -y install -qq google-drive-ocamlfuse fuse
+from google.colab import auth
+auth.authenticate_user()
+from oauth2client.client import GoogleCredentials
+creds = GoogleCredentials.get_application_default()
+import getpass
+!google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL
+vcode = getpass.getpass()
+!echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}
 
-  auth.authenticate_user()
-  drive_service = build('drive', 'v3')
+# tar all files in current directory
+!tar -cvf 'colab_files.tar' .
 
-  def save_file_to_drive(name, path):
-    file_metadata = {'name': name, 'mimeType': 'application/octet-stream'}
-    media = MediaFileUpload(path, mimetype='application/octet-stream', resumable=True)
-    created = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+# save to drive
 
-    return created
+filename = 'colab_files.tar'
 
-  save_file_to_drive(filename, filename)
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
 
-upload_file():
-  # upload a file to colab workspace
-  from google.colab import files
-  uploaded = files.upload()
+auth.authenticate_user()
+drive_service = build('drive', 'v3')
+
+def save_file_to_drive(name, path):
+  file_metadata = {'name': name, 'mimeType': 'application/octet-stream'}
+  media = MediaFileUpload(path, mimetype='application/octet-stream', resumable=True)
+  created = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+  return created
+
+save_file_to_drive(filename, filename)
