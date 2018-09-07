cdfa963d782830361eee416edca3fefd887c1e0f
t0nberryking <tangchiwang@gmail.com>
2018-09-07 18:28:13 +0800

    Create utils.py
    -added auth() and save_all fn

---
+++ utils.py
@@ -0,0 +1,34 @@
+# Authenticate and link to google drive
+def auth():
+  !apt-get install -y -qq software-properties-common python-software-properties module-init-tools
+  !add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
+  !apt-get update -qq 2>&1 > /dev/null
+  !apt-get -y install -qq google-drive-ocamlfuse fuse
+  from google.colab import auth
+  auth.authenticate_user()
+  from oauth2client.client import GoogleCredentials
+  creds = GoogleCredentials.get_application_default()
+  import getpass
+  !google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL
+  vcode = getpass.getpass()
+  !echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}
+
+def save_all(filename):
+  # tar all files in current directory
+  !tar -cvf 'filename' .
+  
+  # save to drive
+  from googleapiclient.http import MediaFileUpload
+  from googleapiclient.discovery import build
+
+  auth.authenticate_user()
+  drive_service = build('drive', 'v3')
+
+  def save_file_to_drive(name, path):
+    file_metadata = {'name': name, 'mimeType': 'application/octet-stream'}
+    media = MediaFileUpload(path, mimetype='application/octet-stream', resumable=True)
+    created = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+    return created
+
+  save_file_to_drive(filename, filename)
